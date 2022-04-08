7fd0851ca7464131d41c2eed405be3ed7528ef4e
Tatiana Vvedenskaya <tatiana.vvedenskaya@exactprosystem.com>
2022-04-08 16:35:49 +0300

    Fixed Context

--- src/main/kotlin/com/exactpro/th2/act/core/dsl/Context.kt
+++ src/main/kotlin/com/exactpro/th2/act/core/dsl/Context.kt
@@ -8,40 +8,70 @@ import com.exactpro.th2.act.core.routers.MessageRouter
 import com.exactpro.th2.act.core.rules.EchoCheckRule
 import com.exactpro.th2.common.grpc.*
 import com.exactpro.th2.common.message.direction
-import com.exactpro.th2.common.message.messageType
 import com.exactpro.th2.common.message.sessionAlias
-import mu.KotlinLogging
 
-private val LOGGER = KotlinLogging.logger {}
+fun context(messageRouter: MessageRouter,
+            responseMonitor: IMessageResponseMonitor,
+            subscriptionManager: SubscriptionManager,
 
-fun context(messageRouter: MessageRouter, responseMonitor: IMessageResponseMonitor, subscriptionManager: SubscriptionManager, action: Context.() -> Unit)/*: ResultBuilder*/{
-    action.invoke(Context(messageRouter, responseMonitor, subscriptionManager))
+            connectivity: Map<String, Direction>,
+            preFilter: (Message) -> Unit = {},
+            action: Context.() -> Unit){
+
+    val sessionAlias = connectivity.keys.toTypedArray()
+    val direction = connectivity.values.toTypedArray()
+
+    if (sessionAlias.size > 1) {
+        action.invoke(
+            Context(messageRouter, responseMonitor, subscriptionManager, sessionAlias[0], direction[0], sessionAlias[1], direction[1])
+        )
+    }
+    else {
+        action.invoke(Context(messageRouter, responseMonitor, subscriptionManager, sessionAlias[0], direction[0]))
+    }
 }
 
-class Context (private val messageRouter: MessageRouter, private val responseMonitor: IMessageResponseMonitor, private val subscriptionManager: SubscriptionManager) { //??
+fun context(messageRouter: MessageRouter,
+            responseMonitor: IMessageResponseMonitor,
+            subscriptionManager: SubscriptionManager,
+
+            sessionAlias: String,
+            anotherSessionAlias: String,
+            preFilter: (Message) -> Unit = {},
+            action: Context.() -> Unit){
+    action.invoke(Context(messageRouter, responseMonitor, subscriptionManager, sessionAlias,  Direction.FIRST, anotherSessionAlias))
+}
+
+class Context(private val messageRouter: MessageRouter,
+              private val responseMonitor: IMessageResponseMonitor,
+              private val subscriptionManager: SubscriptionManager,
+
+              private val sessionAlias: String,
+              private val direction: Direction,
+              private val anotherSessionAlias: String = "",
+              private val anotherDirection: Direction = Direction.FIRST,
+
+              private val preFilter: (Message) -> Unit = {}) {
 
     fun send(message: Message, waitEcho: Boolean = false, cleanBuffer: Boolean = true): Message { //TODO cleanBuffer
-        val sendMessage = messageRouter.sendMessage(message)
+        messageRouter.sendMessage(message)
 
         if (waitEcho) {
-            LOGGER.debug("Started  waiting echo.")
-            val echo = receive(message.sessionAlias, Direction.SECOND) { msg ->
-                passOn(msg, message.messageType) {
-                    msg.direction == Direction.SECOND
-                            && msg.parentEventId == message.parentEventId
+            val echo = receive{
+                passOn(message.metadata.messageType) {
+                    this@Context.sessionAlias == sessionAlias
+                            && direction == Direction.SECOND
+                            && parentEventId == message.parentEventId
                 }
             }
             return echo
         }
-
         return message
     }
 
-    fun receive(sessionAlias: String, direction: Direction, filter: ReceiveBuilder.(Message) -> ReceiveBuilder.Status): Message {
-        LOGGER.debug("Started  receive message.")
-
-        val expectedMessage = Message.newBuilder().apply{
-            this.sessionAlias = sessionAlias
+    fun receive(filter: ReceiveBuilder.() -> Boolean): Message {
+        val expectedMessage = Message.newBuilder().apply {
+            this.sessionAlias = this@Context.sessionAlias
         }.build()
 
         val request = Request(expectedMessage)
@@ -50,7 +80,7 @@ class Context (private val messageRouter: MessageRouter, private val responseMon
         val receiver = MessageReceiver(subscriptionManager, responseMonitor, checkRule, direction)
 
         for (msg in receiver.responseMessages){
-            if (filter.invoke(ReceiveBuilder(), msg) == ReceiveBuilder.Status.PASSED){
+            if (ReceiveBuilder(msg).filter()){
                 return msg
             }
         }
@@ -65,9 +95,8 @@ class Context (private val messageRouter: MessageRouter, private val responseMon
 
     infix fun ((Message) -> Boolean)?.`do`(func: () -> Message): List<Message> {
         val messages = mutableListOf<Message>()
-        var message: Message
         do {
-            message = func.invoke()
+            val message = func.invoke()
             messages.add(message)
         } while (this != null && repeatUntil(message, this) != null)
         return messages
--- src/test/kotlin/com/exactpro/th2/act/dsl/TestDSL.kt
+++ src/test/kotlin/com/exactpro/th2/act/dsl/TestDSL.kt
@@ -1,7 +1,6 @@
 package com.exactpro.th2.act.dsl
 
 import com.exactpro.th2.act.*
-import com.exactpro.th2.act.core.dsl.Context
 import com.exactpro.th2.act.core.dsl.context
 import com.exactpro.th2.act.core.routers.MessageRouter
 import com.exactpro.th2.act.core.managers.SubscriptionManager
@@ -9,7 +8,6 @@ import com.exactpro.th2.act.core.monitors.IMessageResponseMonitor
 import com.exactpro.th2.act.stubs.StubMessageRouter
 import com.exactpro.th2.common.grpc.ConnectionID
 import com.exactpro.th2.common.grpc.Direction
-import com.exactpro.th2.common.grpc.Message
 import com.exactpro.th2.common.grpc.MessageBatch
 import com.exactpro.th2.common.message.direction
 import com.exactpro.th2.common.message.messageType
@@ -37,19 +35,21 @@ class TestDSL {
         subscriptionManager = spyk { }
     }
 
-
     @Test
-    fun `send message and wait echo`(){
-        val context = Context(messageRouter, responseMonitor, subscriptionManager)
+    fun `send message and wait echo`() {
         val message = randomMessage()
-        val connectionID: ConnectionID = ConnectionID.newBuilder().setSessionAlias(message.sessionAlias).build()
-        val echo = context.send(message, true)
-        expect {
-            that(messageBatchRouter.sent.messagesList.size).isEqualTo(1)
-            that(messageBatchRouter.sent.messagesList.first()).apply {
-                get { metadata.id.connectionId }.isEqualTo(connectionID)
-                get { metadata.messageType }.isEqualTo(message.messageType)
-                get { fieldsMap }.isEqualTo(message.fieldsMap)
+        context(messageRouter, responseMonitor, subscriptionManager, mapOf("sessionAlias" to Direction.FIRST)) {
+
+            val connectionID: ConnectionID = ConnectionID.newBuilder().setSessionAlias(message.sessionAlias).build()
+            send(message, true)
+
+            expect {
+                that(messageBatchRouter.sent.messagesList.size).isEqualTo(1)
+                that(messageBatchRouter.sent.messagesList.first()).apply {
+                    get { metadata.id.connectionId }.isEqualTo(connectionID)
+                    get { metadata.messageType }.isEqualTo(message.messageType)
+                    get { fieldsMap }.isEqualTo(message.fieldsMap)
+                }
             }
         }
     }
@@ -57,15 +57,15 @@ class TestDSL {
     @Test
     fun `receive one message`() {
         val message = randomMessage()
-        context(messageRouter, responseMonitor, subscriptionManager) {
+        context(messageRouter, responseMonitor, subscriptionManager, mapOf("sessionAlias" to Direction.FIRST)) {
             send(message)
 
-            val msgReceive = receive("sessionAlias", Direction.FIRST) { msg ->
-                passOn(msg,"NewOrderSingle") {
-                    msg.direction == Direction.FIRST
+            receive {
+                passOn("NewOrderSingle") {
+                    direction == Direction.FIRST
                 }
-                failOn(msg,"NewOrderSingle") {
-                    msg.direction == Direction.SECOND
+                failOn("NewOrderSingle") {
+                    direction == Direction.SECOND
                 }
             }
         }
@@ -74,21 +74,22 @@ class TestDSL {
     @Test
     fun `repeat until`() {
         val message = randomMessage()
-        context(messageRouter, responseMonitor, subscriptionManager){
+        context(messageRouter, responseMonitor, subscriptionManager, "sessionAlias", "anotherSessionAlias"){
             val msg = send(message)
 
             repeatUntil (msg){ mes ->
                 mes.sessionAlias == msg.sessionAlias
             } `do` {
-                receive("sessionAlias", Direction.FIRST) {
-                    passOn(msg, "NewOrderSingle") {
+                receive{
+                    passOn("NewOrderSingle") {
                         msg.direction == Direction.FIRST
                     }
-                    failOn(msg,"NewOrderSingle") {
+                    failOn("NewOrderSingle") {
                         msg.direction == Direction.SECOND
                     }
                 }
             }
         }
     }
+
 }
